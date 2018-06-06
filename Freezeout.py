281d0d23b6ed55c23b97f9a0842f1955219114ba
Rose GRey <rosethegrey@gmail.com>
2018-06-06 14:26:06 +0200

    made functions

--- Freezeout.py
+++ Freezeout.py
@@ -4,4 +4,9 @@ import numpy as np
 import scipy
 
 M_pl = 2.435e18 #GeV/c^2
-H = np.pi/3 * (g_star/10)**(0.5) * T**2/M_pl
+
+def Ht(T):
+    return np.pi/3 * (g_star(T)/10)**(0.5) * T**2/M_pl
+
+def g_star(T):
+    return 10.75
