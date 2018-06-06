07faee9ca5c8a745d6f6daa5070d8f872036e26b
Rose GRey <rosethegrey@gmail.com>
2018-06-06 19:04:47 +0200

    solves dYdx, not checked figures

--- FreezeoutRose.py
+++ FreezeoutRose.py
@@ -1,10 +1,12 @@
 #The Hubble Constant
-
+from __future__ import division
 import numpy as np
 import scipy
 import matplotlib.pyplot as plt
 from sympy.solvers import solve
 from sympy import Symbol
+from scipy.integrate import odeint
+
 
 M_pl = 2.435e18 #GeV/c^2
 
@@ -12,13 +14,14 @@ M_pl = 2.435e18 #GeV/c^2
 class Freezeout(object):
     def __init__(self,  mass = 1):
         self.mass = mass
+        self.Tlist = np.linspace(171, 1, 100)
 
     def H(self, T, g_star = None):
         if g_star is None:
             g_star = self.g_star(T)
         return np.pi/3 * (g_star/10)**(0.5) * T**2/M_pl
 
-    def g_star(self, T):
+    def g_star_int(self, T):
         '''Takes temperature in GeV, returns g_eff, numbers found from https://arxiv.org/pdf/1609.04979.pdf'''
         if T > 170:
             return 106.25
@@ -49,6 +52,40 @@ class Freezeout(object):
         if T <= 0.0005:
             return 3.363
 
+    def g_star(self, Tlist):
+        '''Takes temperature in GeV, returns g_eff, numbers found from https://arxiv.org/pdf/1609.04979.pdf'''
+        gs = []
+        for T in Tlist:
+            if T > 170:
+                gs.append(106.25)
+            if T <= 170 and T > 125: #annihilation of  t, tbar
+                gs.append(96.25)
+            if T <= 125  and T > 91: #annihilation of H0
+                gs.append(95.25)
+            if T <= 91  and T > 80: #annihilation of Z0
+                gs.append(92.25)
+            if T <= 80  and T > 4: #annihilation of W+,W-
+                gs.append(86.25)
+            if T <= 4 and T > 1.8: #annihilation of tau+, tau -
+                gs.append(75.75)
+            if T <= 1.8 and T > 1.2: #annihilation of b bbar
+                gs.append(72.25)
+            if T <= 1.2 and T > 0.2: #annihilation of c, cbar
+                gs.append(61.75)
+            if T <= 0.2 and T > 0.139: #QCD transition
+                gs.append(17.25)
+            if T <= 0.139 and T > 0.131: #annihilation of pi+, pi-
+                gs.append(15.25)
+            if T <= 0.131 and T > 0.106: #annihilation of pi0
+                gs.append(14.25)
+            if T <= 0.106 and T > 0.0008: #annihilation of mu+, mu-
+                gs.append(10.75)
+            if T <= 0.0008 and T > 0.0005: #neutrino decoupling
+                gs.append(6.863)
+            if T <= 0.0005:
+                gs.append(3.363)
+        return gs
+
     def g_starDM(self, mass, dof, Tlist):
         '''takes a descending Tlist and returns geff including extra dof'''
         geff = []
@@ -62,21 +99,32 @@ class Freezeout(object):
             geff = self.g_star(T[n])
         return geff
 
-    def number_density(self, T, limit = 'rel'):
+    def number_density(self, T, limit = 'rel', g_star = None):
+        if g_star is None:
+            g_star = self.g_star(T)
         '''Number density at equilibrium'''
         # non-relativistic m_i >> T
         if limit != 'rel':
-            return 106.25 * (self.mass * T / (2*np.pi))**(3/2) * np.exp(-self.mass/T)
+            return g_star * (self.mass * T / (2*np.pi))**(3/2) * np.exp(-self.mass/T)
         # relativistic m_i << T
         if limit == 'rel':
-            return (106.25* (T)**3)/np.pi**2
+            return (g_star * (T)**3)/np.pi**2
 
-    def dYdx(self, Y, x):
+    def dYdx(self, Y, x, sigv):
         '''The differential equation for dY/dx, Y = nx/T^3, x = m/T'''
-        sigv = 1e-30
-        Yeq = self.number_density(self.Tlist) / (self.Tlist)**3
-        lambd = (self.mass)**3 * sigv / H(self.mass)
-        return (lambd / x**2) * (Y - Yeq)
+        return ((self.mass)**3 * sigv / self.H(self.mass, g_star = self.g_star_int(self.mass)) / x**2) * (Y - self.number_density(self.mass/x, g_star = self.g_star_int(self.mass/x)) / (self.mass/x)**3)
+
+
+    def getY(self, xlist = None):
+        '''Solves dYdx'''
+        if xlist is None:
+            xlist = self.mass / self.Tlist
+        T0 = 200
+        Y0 = self.number_density(T0, g_star = self.g_star_int(T0)) / T0**3
+        print(Y0)
+        Y = odeint(self.dYdx, Y0, xlist, args = (1e-20,))[:,0]
+        #return lambda x: np.interp(x, xlist, Y)
+        return Y
 
     def xfreeze(self, sigv):
         Yinf = 1 #Y at very late times,
@@ -84,4 +132,4 @@ class Freezeout(object):
 
     def Tfreeze(self, sigv):
         T = Symbol('T')
-        return solve(self.H(T, 106.25) == self.number_density(T) * sigv, T)
+        return solve(self.H(T, 106.25) == self.number_density(T, g_star = 106.25) * sigv, T)
